4572f5fcb3623ea7b950340d01e0919c703bb99a
Juan Martinez <pablomar01@outlook.com>
2024-01-15 15:26:57 -0500

    login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,5 +2,31 @@
 </script>
 
 <template>
-  <h1 class="text-center text-h3 font-weight-bold my-5">Login</h1>
+  <v-card flat max-width="600" class="mx-auto">
+    <v-card-title class="text-h4 font-weight-bold" tag="h3">
+      Login
+    </v-card-title>
+
+    <v-card-subtitle>
+      Login with your account
+    </v-card-subtitle>
+
+    <v-form class="mt-5">
+      <v-text-field 
+        type="email"
+        label="Email"
+        bg-color="blue-grey-lighten-5"
+      />
+
+      <v-text-field 
+        type="password"
+        label="Password"
+        bg-color="blue-grey-lighten-5"
+      />
+
+      <v-btn block flat color="pink-accent-3">
+        Login
+      </v-btn>
+    </v-form>
+  </v-card>
 </template>
