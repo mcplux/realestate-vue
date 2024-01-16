923e814f5cbdae9a3c9f1ca7c63042604b523034
Juan Martinez <pablomar01@outlook.com>
2024-01-16 15:07:30 -0500

    show error messages

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,19 +1,31 @@
 import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
 import { useFirebaseAuth } from 'vuefire';
 import { signInWithEmailAndPassword } from 'firebase/auth'
 
 export const useAuthStore = defineStore('auth', () => {
   const auth = useFirebaseAuth()
+  const errorMsg = ref('')
+  const errorCode = {
+    'auth/invalid-credetial': 'Invalid credentials',
+    'default': 'An error occurred',
+  }
 
   const login = ({email, password}) => {
     signInWithEmailAndPassword(auth, email, password)
     .then((userCredentials) => {
       console.log(userCredentials)
     })
-    .catch(error => console.log(error.code)) 
+    .catch(error => {
+      errorMsg.value = errorCode[error.code] ?? errorCode['default']
+    }) 
   }
+
+  const hasError = computed(() => errorMsg.value)
 
   return {
     login,
+    hasError,
+    errorMsg,
   }
 })
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -24,6 +24,13 @@ const sumbit = handleSubmit(values => {
       Login with your account
     </v-card-subtitle>
 
+    <v-alert
+      v-if="auth.hasError"
+      class="my-5"
+      type="error"
+      :title="auth.errorMsg"
+    ></v-alert>
+
     <v-form class="mt-5">
       <v-text-field 
         type="email"
