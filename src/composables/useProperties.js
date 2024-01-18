d8d9baf36a1365c464dd60b7636afd78c2029f57
Juan Martinez <pablomar01@outlook.com>
2024-01-18 18:14:29 -0500

    filter by pool

--- src/composables/useProperties.js
+++ src/composables/useProperties.js
@@ -1,11 +1,18 @@
+import { computed, ref } from 'vue'
 import { collection } from 'firebase/firestore'
 import { useFirestore, useCollection } from 'vuefire'
 
 export default function useProperties() {
+  const pool = ref(false)
+
   const db = useFirestore()
   const propertiesCollection = useCollection(collection(db, 'properties'))
 
+  const filteredProperties = computed(() => pool.value ? propertiesCollection.value.filter(property => property.pool) : propertiesCollection.value)
+
   return {
     propertiesCollection,
+    pool,
+    filteredProperties,
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,24 @@ import useProperties from '../composables/useProperties'
 import Property from '../components/Property.vue'
 import { formatPrice } from '../helpers'
 
-const { propertiesCollection } = useProperties()
+const { pool, filteredProperties } = useProperties()
 </script>
 
 <template>
   <h1 class="text-center text-h4 font-weight-bold my-5">Our properties</h1>
 
   <v-card flat class="py-10">
+    <v-card-title class="text-h5">
+      Filter
+      <v-checkbox 
+        label="Pool"
+        v-model="pool"
+      />
+    </v-card-title>
+
     <v-row>
       <Property 
-        v-for="property in propertiesCollection"
+        v-for="property in filteredProperties"
         :key="property.id"
         :property="property"
         :formatPrice="formatPrice"
